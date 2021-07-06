76ce9ec5ae33d59328b2db7576583f798fa6385d
Vitaliy Kudryk <kudryk.v@protonmail.com>
2021-07-06 09:25:32 +0300

    update few titles; use CGO_ENABLED=1 to build binaries

--- release.sh
+++ release.sh
@@ -17,7 +17,7 @@ echo "Prep to build for OSes ${_gooses[*]} with arches ${_goarches[*]}"
 for _goos in "${_gooses[@]}"; do
   for _goarch in "${_goarches[@]}"; do
     echo -n "Building for OS ${_goos} arch ${_goarch}... "
-    GOOS=${_goos} GOARCH=${_goarch} go build --tags fts5 -o run ./app
+    GOOS=${_goos} GOARCH=${_goarch} CGO_ENABLED=1 go build --tags fts5 -o run ./app
     echo "built"
 
     _zipname="CraftDocs_SearchIndex_${_version}_${_goos}_${_goarch}.alfredworkflow"
